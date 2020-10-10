c889056eeea308671ee1ab32e8d92ec9489249b2
meghamore1612 <53705664+meghamore1612@users.noreply.github.com>
2020-10-10 21:28:52 +0530

    Assignment Day_2

---
+++ Employee.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Employee {
+
+	String name="Saurab";
+	int age=23;
+	String city="Chennai";
+	
+	public void showEmpDetails()
+	{
+		System.out.println("The name is:"+name);
+		System.out.println("The age is:"+age);
+		System.out.println("The city is:"+city);
+	
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee e=new Employee();
+		e.showEmpDetails();
+	
+		Employee e1=new Employee();
+		e1.showEmpDetails();
+		
+	}
+
+}
