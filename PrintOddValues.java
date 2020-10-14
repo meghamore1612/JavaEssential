298a3c94eab650b3cc1206dd9965e52b4ae0ae25
meghamore1612 <53705664+meghamore1612@users.noreply.github.com>
2020-10-14 17:33:58 +0530

    Assignment _day4

---
+++ PrintOddValues.java
@@ -0,0 +1,18 @@
+package assignment_day4;
+
+public class PrintOddValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int a[]={1,2,5,6,3,2};  
+		System.out.println("Odd Numbers:");  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2!=0){  
+		System.out.println(a[i]);  
+	}
+	}
+	}
+}
